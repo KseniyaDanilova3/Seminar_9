d3168d410d53cd0a058d62eabb6621c8bde3e1c1
Kseniya <pashkina.ks@mail.ru>
2022-10-01 17:59:25 +0700

    задача_67

---
+++ Задача_67/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(GetNumbersSum(number));
+
+// возвращает сумму цифр
+int GetNumbersSum(int number) 
+{
+      if (number == 0)
+            return number % 10;     // возвращает остаток от деления (последний разряд)
+
+      return GetNumbersSum(number / 10) + number % 10;
+}
